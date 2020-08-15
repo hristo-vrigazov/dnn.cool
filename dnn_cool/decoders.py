5b621be4606e5cf1c7d11a51fbce0de76910f5e1
Hristo Vrigazov <hvrigazov@gmail.com>
2020-08-15 17:35:44 +0300

    Implement multi-label classification decoder.

--- dnn_cool/decoders.py
+++ dnn_cool/decoders.py
@@ -136,14 +136,29 @@ def sort_declining(x):
 
 class MultilabelClassificationDecoder(Decoder):
 
-    def __init__(self):
+    def __init__(self, metric=accuracy_score):
         self.thresholds = torch.ones(3).unsqueeze(0) * 0.5
+        self._candidates = np.linspace(0., 1., num=100)
+        self.metric = metric
 
     def __call__(self, x):
+        if isinstance(x, np.ndarray):
+            return x > self.thresholds.cpu().numpy()
         return x > self.thresholds.to(x.device)
 
     def tune(self, predictions, targets):
-        raise NotImplementedError()
+        n_classes = predictions.shape[1]
+        res = np.zeros((n_classes, len(self._candidates)), dtype=float)
+        for class_idx in range(n_classes):
+            for j, candidate in enumerate(tqdm(self._candidates)):
+                preds = (predictions[:, class_idx] > candidate)
+                gt = targets[:, class_idx]
+                res[class_idx, j] = self.metric(preds, gt)
+
+        best_threshold_indices = res.argmax(axis=1)
+        params = { 'thresholds': self._candidates[best_threshold_indices] }
+        self.load_tuned(params)
+        return params
 
     def load_tuned(self, params):
-        raise NotImplementedError()
+        self.thresholds = torch.tensor(params['thresholds']).unsqueeze(0)
