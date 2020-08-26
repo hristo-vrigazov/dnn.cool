e9bae1b4a60cdf9aa87201c90d09c618cc7fe8d5
Hristo Vrigazov <hvrigazov@gmail.com>
2020-08-26 09:35:47 +0300

    Fix hardcoded threshold for the multi label decoder.

--- dnn_cool/decoders.py
+++ dnn_cool/decoders.py
@@ -136,18 +136,21 @@ def sort_declining(x):
 
 class MultilabelClassificationDecoder(Decoder):
 
-    def __init__(self, n_classes=3, metric=accuracy_score):
-        self.thresholds = torch.ones(n_classes).unsqueeze(0) * 0.5
+    def __init__(self, metric=accuracy_score):
+        self.thresholds = None
         self._candidates = np.linspace(0., 1., num=100)
         self.metric = metric
 
     def __call__(self, x):
+        if self.thresholds is None:
+            n_classes = x.shape[-1]
+            self.thresholds = torch.ones(n_classes).unsqueeze(0) * 0.5
         if isinstance(x, np.ndarray):
             return x > self.thresholds.cpu().numpy()
         return x > self.thresholds.to(x.device)
 
     def tune(self, predictions, targets):
-        n_classes = predictions.shape[1]
+        n_classes = predictions.shape[-1]
         res = np.zeros((n_classes, len(self._candidates)), dtype=float)
         for class_idx in range(n_classes):
             for j, candidate in enumerate(tqdm(self._candidates)):
