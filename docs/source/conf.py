59a6f5f8e66e75475b81dd6082c9b74a3cdfb35c
Hristo Vrigazov <hvrigazov@gmail.com>
2020-09-22 21:37:49 +0300

    Incremented nano version.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Hristo Vrigazov'
 author = 'Hristo Vrigazov'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.3'
+release = '0.1.3.1'
 
 
 # -- General configuration ---------------------------------------------------
