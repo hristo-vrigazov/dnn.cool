29d3810ceb10f4f607ff3804c1e39116908a65df
Hristo Vrigazov <hvrigazov@gmail.com>
2020-11-01 17:25:20 +0200

    Make it possible to use rf API in multi-input, multi-output setting.

--- dnn_cool/rf.py
+++ dnn_cool/rf.py
@@ -3,25 +3,73 @@ import torch
 from torch import nn
 
 
+def dummy_tensor(inputs):
+    return nn.Parameter(data=torch.ones_like(inputs))
+
+
+def dummy_dict(inputs):
+    res = nn.ParameterDict()
+    for key, value in inputs.items():
+        if key != 'gt' and not key.startswith('precondition'):
+            res[key] = dummy_tensor(value)
+    return res
+
+
 class RFModel(nn.Module):
 
     def __init__(self, inputs, model):
         super().__init__()
-        # TODO: handle dict input
-        self.dummy = nn.Parameter(data=torch.ones_like(inputs))
+        self.dummy = dummy_dict(inputs) if isinstance(inputs, dict) else dummy_tensor(inputs)
         self.model = model
 
     def forward(self, x):
-        return self.model(self.dummy * x)
+        if isinstance(x, torch.Tensor):
+            return self.model(self.dummy * x)
+        for key, value in self.dummy.items():
+            x[key] *= value
+        return self.model(x)
 
 
-def compute_receptive_field(model, inputs, out_mask, marker=5000., eps=1e-3):
+def compute_receptive_field(model, inputs, out_mask, marker=5e8):
     rf_model = RFModel(inputs, model)
     pred = rf_model(inputs)
-    target = pred.detach().clone()
-    target[out_mask] += marker
-    mse = nn.MSELoss()
-    loss = mse(pred, target)
-    loss.backward()
-    rf_mask = abs(rf_model.dummy.grad) > eps
+    target = mark_predictions(pred, out_mask, marker)
+    rf_backprop(pred, target)
+    rf_mask = get_rf_mask(rf_model)
     return rf_mask
+
+
+def get_rf_mask(rf_model, eps=1e-4):
+    if isinstance(rf_model.dummy, torch.Tensor):
+        return abs(rf_model.dummy.grad) > eps
+    rf_mask = {}
+    for key, value in rf_model.dummy.items():
+        rf_mask[key] = abs(value.grad) > eps
+    return rf_mask
+
+
+def rf_backprop(pred, target):
+    if isinstance(pred, torch.Tensor):
+        mse = nn.MSELoss()
+        loss = mse(pred, target)
+        loss.backward()
+        return
+    mse = nn.MSELoss()
+    losses = []
+    for key, value in pred.items():
+        losses.append(mse(value, target[key]))
+    loss = torch.stack(losses).mean()
+    loss.backward()
+
+
+def mark_predictions(pred, out_mask, marker):
+    if isinstance(pred, torch.Tensor):
+        target = pred.detach().clone()
+        target[out_mask] += marker
+        return target
+    assert isinstance(pred, dict)
+    target = {}
+    for key, value in pred.items():
+        target[key] = value.detach().clone()
+        target[key][out_mask[key]] += marker
+    return target
--- tests/test_rf.py
+++ tests/test_rf.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from torch import nn
 
 from dnn_cool.rf import compute_receptive_field
+from dnn_cool.synthetic_dataset import synthetic_dataset_preparation
 
 
 def test_rf_simple_conv():
@@ -41,4 +42,44 @@ def test_rf_simple_summing():
 
     plt.matshow(out_mask)
     plt.show()
+
+
+def test_rf_multi_input():
+    model, nested_loaders, datasets, project = synthetic_dataset_preparation()
+    runner = project.runner(model=model, runner_name='default_experiment', balance_dataparallel_memory=False)
+    loader = nested_loaders['valid']
+    X, y = next(iter(loader))
+    X = runner.batch_to_model_device(X)
+    y = runner.batch_to_model_device(y)
+
+    class FeaturesModel(nn.Module):
+
+        def __init__(self, model):
+            super().__init__()
+            self.model = model
+
+        def forward(self, x):
+            features = self.model.seq[:4](x['syn_img'])
+            return {
+                'features': features
+            }
+
+    model = FeaturesModel(model).eval()
+    with torch.no_grad():
+        res = model(X)
+        out_mask = {key: torch.zeros_like(value).bool() for key, value in res.items()}
+        out_mask['features'][:, :, 11, 11] = True
+
+    rf_mask = compute_receptive_field(model, X, out_mask)
+
+    nonzero_indices = (torch.nonzero(rf_mask['syn_img']))
+    mins = nonzero_indices.min(dim=0).values
+    maxs = nonzero_indices.max(dim=0).values + 1
+    lengths = maxs - mins
+
+    assert lengths[0].item() == loader.batch_size
+    assert lengths[1].item() == 3
+    assert lengths[2].item() == 9
+    assert lengths[3].item() == 9
+
 
