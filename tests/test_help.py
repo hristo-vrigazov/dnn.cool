4b7a6f4aa457c04abf297f75346931be87112e2b
Hristo Vrigazov <hvrigazov@gmail.com>
2021-10-03 13:58:57 +0300

    Added more steps in the help test.

--- tests/test_help.py
+++ tests/test_help.py
@@ -8,3 +8,8 @@ def test_help_tasks():
     task = BinaryClassificationTask('task', None)
 
     tasks = Tasks([task])
+
+    @tasks.add_flow
+    def task_flow(flow, x, out):
+        out += flow.task(x.features)
+        return out
