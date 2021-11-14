82f2c8a7785fe0af87cdfee9353615c56a4755e5
Hristo Vrigazov <hvrigazov@gmail.com>
2021-11-14 11:29:37 +0200

    Another example.

--- dnn_cool/collators/base.py
+++ dnn_cool/collators/base.py
@@ -1,3 +1,6 @@
+from dataclasses import dataclass
+from typing import Dict
+
 
 def initialize_dict_structure(dct):
     res = {}
@@ -24,22 +27,45 @@ def find_padding_shape_of_nested_list(ll):
     return [len(ll)] + find_max_len_of_list_of_lists(ll)
 
 
-def append_example_to_dict(dct, shapes, ex):
+def append_example_to_dict(dct, ex):
     for key, value in ex.items():
         if isinstance(value, dict):
-            dct[key] = append_example_to_dict(dct[key], shapes[key], value)
+            dct[key] = append_example_to_dict(dct[key], value)
             continue
         dct[key].append(value)
     return dct
+
+
+def create_shapes_dict(dct):
+    res = {}
+    for key, value in dct.items():
+        if isinstance(value, dict):
+            res[key] = initialize_dict_structure(value)
+            continue
+        res[key] = find_padding_shape_of_nested_list(value)
+    return res
+
+
+@dataclass
+class CollatorData:
+    X_batch: Dict
+    y_batch: Dict
+    X_shapes: Dict
+    y_shapes: Dict
 
 
 def examples_to_nested_list(examples):
     X_ex, y_ex = examples[0]
     X_batch = initialize_dict_structure(X_ex)
     y_batch = initialize_dict_structure(y_ex)
-
-    X_shapes = initialize_dict_structure(X_ex)
-    y_shapes = initialize_dict_structure(y_ex)
-
     for example in examples:
         X_ex, y_ex = example
+        append_example_to_dict(X_batch, X_ex)
+        append_example_to_dict(y_batch, y_ex)
+
+    return CollatorData(
+        X_batch=X_batch,
+        y_batch=y_batch,
+        X_shapes=create_shapes_dict(X_batch),
+        y_shapes=create_shapes_dict(y_batch)
+    )
--- tests/test_collator.py
+++ tests/test_collator.py
@@ -1,6 +1,6 @@
 import torch
 
-from dnn_cool.collators.base import find_padding_shape_of_nested_list
+from dnn_cool.collators.base import find_padding_shape_of_nested_list, examples_to_nested_list
 
 
 def test_finds_max_along_every_axis():
@@ -30,13 +30,24 @@ def test_finds_max_along_every_axis_3_axes():
 
 
 def test_finds_max_along_every_axis_5_axes():
+    ll = create_nested_list_test()
+    res = find_padding_shape_of_nested_list(ll)
+    assert res == [3, 2, 6, 1, 5]
+
+
+def create_nested_list_test():
     ll000 = [torch.tensor([1, 2, 3, 4, 5])]
     ll010 = [torch.tensor([1])]
     ll00 = [ll000, ll000, ll000, ll000, ll000, ll000]
     ll01 = [ll010]
     ll0 = [ll00, ll01]
     ll = [ll0, ll0, ll0]
-    res = find_padding_shape_of_nested_list(ll)
-    assert res == [3, 2, 6, 1, 5]
+    return ll
 
 
+def test_examples_to_nested_list():
+    examples = [
+        ({'task_name': create_nested_list_test()}, {'label': torch.tensor([5])})
+    ]
+    r = examples_to_nested_list(examples)
+    print(r)
